3821ebd6a07ee853994d772e8f2feeeb89a339e6
Slawek Ludwiczak <slawek@javastart.pl>
2018-07-10 10:51:33 +0200

    spring-mvc update to spring 5

--- spring-mvc/src/main/java/pl/javastart/WebConfig.java
+++ spring-mvc/src/main/java/pl/javastart/WebConfig.java
@@ -6,13 +6,13 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @ComponentScan
 @EnableWebMvc
-public class WebConfig extends WebMvcConfigurerAdapter {
+public class WebConfig implements WebMvcConfigurer {
     
     @Bean
     public ViewResolver viewResolver() {
