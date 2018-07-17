c049dd91f54d142dd98f14b318bcd336f51860db
Slawek Ludwiczak <slawek@javastart.pl>
2018-07-17 14:35:25 +0200

    spring-validator-mvc updated to java10 and boot2

--- spring-validator-mvc/src/main/java/pl/javastart/model/Person.java
+++ spring-validator-mvc/src/main/java/pl/javastart/model/Person.java
@@ -1,9 +1,8 @@
 package pl.javastart.model;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.Min;
-
-import org.hibernate.validator.constraints.Email;
-import org.hibernate.validator.constraints.NotEmpty;
+import javax.validation.constraints.NotEmpty;
 
 public class Person {
 
