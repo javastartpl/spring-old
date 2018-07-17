123d49fea111c5cda708cedb9ee81d992ad3ec55
Slawek Ludwiczak <slawek@javastart.pl>
2018-07-17 14:29:47 +0200

    spring-validator-jpa updated to java10 and boot2

--- spring-validator-jpa/src/main/java/pl/javastart/model/Person.java
+++ spring-validator-jpa/src/main/java/pl/javastart/model/Person.java
@@ -4,10 +4,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.Min;
-
-import org.hibernate.validator.constraints.Email;
-import org.hibernate.validator.constraints.NotEmpty;
+import javax.validation.constraints.NotEmpty;
 
 @Entity
 public class Person {
