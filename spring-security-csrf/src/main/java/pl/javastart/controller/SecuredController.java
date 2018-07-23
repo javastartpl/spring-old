b0effd906ba1b9559c6a5b4085e45d1ca4b3ed47
Slawek Ludwiczak <slawek@javastart.pl>
2018-07-23 14:59:51 +0200

    spring-security-csrf updated to boot2 and java 10

--- spring-security-csrf/src/main/java/pl/javastart/controller/SecuredController.java
+++ spring-security-csrf/src/main/java/pl/javastart/controller/SecuredController.java
@@ -1,24 +1,23 @@
 package pl.javastart.controller;
 
-import javax.servlet.http.HttpSession;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class SecuredController {
     
     @RequestMapping("/")
-    public String home(HttpSession session) {
+    public String home() {
         return "index";
     }
 
     @PostMapping("/calculate")
+    @ResponseBody
     public String calculate(@RequestParam int a, @RequestParam int b) {
-        System.out.println("a + b =" + (a + b));
-        return "redirect:/";
+        return String.format("%d + %d = %d", a, b, (a+b));
     }
     
 }
