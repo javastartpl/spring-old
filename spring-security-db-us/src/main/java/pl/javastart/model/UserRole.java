224f5b6495f3698a1221fe3fb11060337de7d755
slawek <slawekwch@gmail.com>
2020-10-13 14:24:40 +0200

    fixed id generation

--- spring-security-db-us/src/main/java/pl/javastart/model/UserRole.java
+++ spring-security-db-us/src/main/java/pl/javastart/model/UserRole.java
@@ -2,13 +2,14 @@ package pl.javastart.model;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class UserRole {
 	
 	@Id
-	@GeneratedValue
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String role;
 	private String description;
