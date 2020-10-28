af375245b98e65cc20e3f98ab0d5a2461556e07f
slawek <slawekwch@gmail.com>
2020-10-28 08:48:50 +0100

    updated java and spring version

--- spring-angular-security/src/main/java/pl/javastart/api/BookEndpoint.java
+++ spring-angular-security/src/main/java/pl/javastart/api/BookEndpoint.java
@@ -33,12 +33,9 @@ public class BookEndpoint {
     
     @GetMapping("/api/books/{id}")
     public ResponseEntity<Book> getById(@PathVariable Long id) {
-        Book book = bookRepository.findOne(id);
-        if(book != null) {
-            return ResponseEntity.ok(book);
-        } else {
-            return ResponseEntity.notFound().build();
-        }
+        return bookRepository.findById(id)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
     
     @PostMapping("/api/books")
